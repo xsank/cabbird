560262e215c0b6324c7f3b8d62f00b6886150016
xsank <825128975@qq.com>
2014-08-24 18:16:31 +0800

    modify the code

--- bit/max_one_num.py
+++ bit/max_one_num.py
@@ -1,9 +1,8 @@
 
 def count_max_one(num=0):
     sum=0
-    n=num
-    while n:
-        n&=n-1
+    while num:
+        num&=num-1
         sum+=1
     return sum
 
--- bit/max_serial_one_num.py
+++ bit/max_serial_one_num.py
@@ -1,10 +1,9 @@
 
 def count_max_serial_one(num=0):
-    n=num
     res=tmp=0
-    while n:
-        tmp=tmp+1 if n&1 else 0
+    while num:
+        tmp=tmp+1 if num&1 else 0
         res=max(res,tmp)
-        n>>=1
+        num>>=1
     return res
 
