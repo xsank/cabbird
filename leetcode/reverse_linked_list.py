045cc38e72d3104f14de2228348819dc36de8108
xsank <825128975@qq.com>
2015-06-18 13:05:20 +0800

    add easy leetcode

---
+++ leetcode/reverse_linked_list.py
@@ -0,0 +1,16 @@
+from structure.listnode import *
+
+def reverseList(head):
+    _head=p=ListNode(-1)
+    p=head
+    while p:
+        t=_head.next
+        tt=p.next
+        _head.next=p
+        p.next=t
+        p=tt
+    return _head.next
+
+if __name__=="__main__":
+    _list=listToNode([])
+    printNode(reverseList(_list))
