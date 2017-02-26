446e4e72832047ce0efd4a0f147266353961594e
孔天逸 <598515595@qq.com>
2017-02-26 16:23:57 +0800

    修改了一些bug
    1. 在字典和列表最后出现逗号会使json.load()函数无法读取，故应做个判断。
    2. 消除在文件开头无用的空格。

--- recursive/json_formatter.py
+++ recursive/json_formatter.py
@@ -3,11 +3,6 @@
 
 class JsonFormatter:
     def __init__(self, intend=4, name="", encoding="utf-8"):
-        '''
-        intend: 缩进空格数
-        name: 文件名
-        encoding: 文件编码
-        '''
         self.name = name
         self.intend = intend
         self.encoding = encoding
@@ -18,22 +13,17 @@ class JsonFormatter:
 
     @staticmethod
     def json_str(s):
-        '''
-        给字符串套上双引号
-        '''
         return '"' + s + '"'
 
     @staticmethod
     def get_source(name, encoding="utf-8"):
         with open(name, 'r', encoding=encoding) as f:
-            # 当不给split函数传递任何参数时，分隔符sep会采用任意形式的空白字符：空格、tab、换行、回车以及换页符
             return ''.join(f.read().split())
 
     def prepare(self):
         try:
             self.obj = eval(self.source)
         except:
-            # json string 一定满足python dict和list的组合
             raise Exception('Invalid json string!')
 
     def line_intend(self, level=0):
